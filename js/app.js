67370807482519f752e3a1c6e22c667460b160a1
Joao Felipe <j155c137@gmail.com>
2024-11-04 22:55:04 -0300

    adicionando popup para forma de pagamento

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
-const botaoAbrirPopup = document.getElementById('botao_add');
+const botaoAbrirAdd = document.getElementById('botao_add');
+const botaoAbrirFormPag = document.getElementById('botao_formPag')
 const botaoFecharPopup = document.getElementById('fecharPopup');
-const popup = document.getElementById('popup');
+const popup_addProd = document.getElementById('popup_addProd');
+const popup_addFormPag = document.getElementById('popup_addFormPag');
 
-botaoAbrirPopup.addEventListener('click', () => {
-    popup.style.display = 'block';
+botaoAbrirAdd.addEventListener('click', () => {
+    popup_addProd.style.display = 'block';
+});
+
+botaoAbrirFormPag.addEventListener('click', () => {
+    popup_addFormPag.style.display = 'block';
 });
 
 botaoFecharPopup.addEventListener('click', () => {
-    popup.style.display = 'none';
+    popup_addProd.style.display = 'none';
 });
 
 window.addEventListener('click', (event) => {
-    if (event.target === popup) {
-        popup.style.display = 'none';
+    if (event.target === popup_addProd | event.target === popup_addFormPag) {
+        popup_addProd.style.display = 'none';
+        popup_addFormPag.style.display = 'none';
     }
 });
