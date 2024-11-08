b78e50a47d9d04632935bf392e8764fafef30dc7
Joao Felipe <j155c137@gmail.com>
2024-11-08 11:02:07 -0300

    Adicionando o popup de desconto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const botaoAbrirAdd = document.getElementById('botao_add');
-const botaoAbrirFormPag = document.getElementById('botao_formPag')
+const botaoAbrirFormPag = document.getElementById('botao_formPag');
+const botaoAbrirDesc = document.getElementById('botao_desc');
 const botaoFecharPopup = document.getElementById('fecharPopup');
+
 const popup_addProd = document.getElementById('popup_addProd');
 const popup_addFormPag = document.getElementById('popup_addFormPag');
+const popup_addDesc = document.getElementById('popup_addDesc');
+
+
 
 botaoAbrirAdd.addEventListener('click', () => {
     popup_addProd.style.display = 'block';
@@ -12,13 +17,14 @@ botaoAbrirFormPag.addEventListener('click', () => {
     popup_addFormPag.style.display = 'block';
 });
 
-botaoFecharPopup.addEventListener('click', () => {
-    popup_addProd.style.display = 'none';
+botaoAbrirDesc.addEventListener('click', () => {
+    popup_addDesc.style.display = 'block';
 });
 
 window.addEventListener('click', (event) => {
-    if (event.target === popup_addProd | event.target === popup_addFormPag) {
+    if (event.target === popup_addProd | event.target === popup_addFormPag | event.target === popup_addDesc) {
         popup_addProd.style.display = 'none';
         popup_addFormPag.style.display = 'none';
+        popup_addDesc.style.display = 'none';
     }
 });
